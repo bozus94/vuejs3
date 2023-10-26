4662d8d3f3903d3969469702b7fd9558e80f72b0
Bryan Ortega <bj_ortega@hotmail.com>
2023-10-26 13:34:59 +0200

    WIP: Trabajando con pinia

---
+++ global-pinia/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<h1>{{ store.count }} x 2 = {{ store.time2 }}</h1>
+		<button @click="store.increment(1)">Increment</button>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from "@/store/useCounterStore";
+
+const store = useCounterStore();
+</script>
---
+++ global-pinia/src/store/useCounterStore.ts
@@ -0,0 +1,17 @@
+import { defineStore } from "pinia";
+
+export const useCounterStore = defineStore("counter", {
+	state: () => {
+		return {
+			count: 1,
+		};
+	},
+	getters: {
+		time2: (state) => state.count * 2,
+	},
+	actions: {
+		increment(val = 1) {
+			this.count += val;
+		},
+	},
+});
