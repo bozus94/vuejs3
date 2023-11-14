a45c5a12b7091b571cbe38afd062dc9ec1ed4994
Bryan Ortega <bj_ortega@hotmail.com>
2023-11-14 20:55:00 +0100

    WIP: composables

---
+++ reusable/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+	<TailwindView>
+		<template #titleHeader>Home</template>
+		<div class="home">
+			<img alt="Vue logo" src="../assets/logo.png" />
+			<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+		</div>
+	</TailwindView>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import TailwindView from "./layouts/TailwindView.vue";
+export default defineComponent({
+	name: "HomeView",
+	components: {
+		HelloWorld,
+		TailwindView,
+	},
+});
+</script>
---
+++ reusable/src/views/layouts/TailwindView.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="min-h-full">
+		<NavBar></NavBar>
+		<header class="bg-white shadow">
+			<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+				<h1 class="text-3xl font-bold tracking-tight text-gray-900">
+					<slot name="titleHeader"></slot>
+				</h1>
+			</div>
+		</header>
+		<main>
+			<div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+				<slot></slot>
+			</div>
+		</main>
+		<FooterBasic></FooterBasic>
+	</div>
+</template>
+<script lang="ts" setup>
+import NavBar from "@/components/NavBar.vue";
+import FooterBasic from "@/components/FooterBasic.vue";
+</script>
